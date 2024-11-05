58792e9e7b7c1f2805a6b1f36e617a730e58bb18
german4705 <61360503+german4705@users.noreply.github.com>
2024-11-05 11:31:15 -0300

    fov

---
+++ practicar-main/Assets/SCRIPTS/ParcialIA/EnemyFOV.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFOV : MonoBehaviour
+{
+    [SerializeField] float viewRadius;
+    [SerializeField] float viewAngle;
+
+    [SerializeField] GameObject player;
+    [SerializeField] LayerMask wallLayer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fieldOfView(player))
+        {
+            //enemy.GetComponent<Renderer>().material.color = Color.red;
+
+        }
+        else
+        {
+            //enemy.GetComponent<Renderer>().material.color = Color.white;
+            Debug.DrawLine(transform.position, player.transform.position, Color.red);
+        }
+    }
+
+    public bool fieldOfView(GameObject obj)
+    {
+        Vector3 dir = obj.transform.position - transform.position;
+
+        if (dir.magnitude < viewRadius)
+        {
+            Debug.Log("dentro de la pizza");
+            if (Vector3.Angle(transform.forward, dir) < viewAngle / 2)
+            {
+                if (!Physics.Raycast(transform.position, dir, dir.magnitude, wallLayer))
+                {
+                    Debug.DrawLine(transform.position, obj.transform.position, Color.blue);
+                    return true;
+
+                }
+
+
+            }
+
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+
+        Vector3 LineA = GetVectorFromAngle(viewAngle / 2 + transform.eulerAngles.y);
+        Vector3 LineB = GetVectorFromAngle(-viewAngle / 2 + transform.eulerAngles.y);
+
+        Gizmos.DrawLine(transform.position, transform.position + LineA * viewRadius);
+        Gizmos.DrawLine(transform.position, transform.position + LineB * viewRadius);
+    }
+
+    public Vector3 GetVectorFromAngle(float angle)
+    {
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+    }
+}
